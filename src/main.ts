2bee1d0661510707ae6632beb7cff0eb99fc5e3f
Nurali11 <boltaboyevnurali55@gmal.com>
2025-04-21 14:48:12 +0500

    done

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log("fds");
+
   
   const config = new DocumentBuilder()
     .setTitle('Cats example')
     .setDescription('The cats API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addSecurityRequirements('bearer', ['bearer'])
+    .addBearerAuth()
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs ', app, documentFactory);
+  SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
-
